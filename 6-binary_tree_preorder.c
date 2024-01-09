ff6e091663a74d6b95c698df518796897fcea3fa
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-01-09 16:31:47 -0300

    Update 6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - goes through a binary tree
+ * binary_tree_preorder - goes through a binary tree
  * @tree: a pointer to the root node
  * @func: a pointer to the function
-**/
-
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-		if (!tree || !func)
-		return;
-		func(tree->n);
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
+    if (!tree || !func)
+        return;
+
+    func(tree->n);
+    binary_tree_preorder(tree->left, func);
+    binary_tree_preorder(tree->right, func);
 }
