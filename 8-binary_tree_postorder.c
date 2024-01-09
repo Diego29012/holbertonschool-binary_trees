d61adb7c1165b19295ddf0f76a0cb9ebb4275789
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-01-09 13:18:00 -0300

    Update 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -4,14 +4,13 @@
  * binary_tree_postorder - goes through a binary tree
  * @tree: a pointer to the root node
  * @func: a pointer to the function
-*/
+**/
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
+		if (!tree || !func)
 		return;
-
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
-	func(tree->n);
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
 }
